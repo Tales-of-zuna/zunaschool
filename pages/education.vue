937b98909e3a88de61bb79288d62fc1ba77efed7
Byambadulam <bmbdlm2001@gmail.com>
2022-10-09 20:36:11 +0800

    firebase suulgahas omno XD

---
+++ pages/education.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-sheet>
+    <v-parallax
+      src="https://c0.wallpaperflare.com/preview/181/960/841/cambridge-united-states-harvard-university-library.jpg"
+      height="300"
+    ></v-parallax>
+
+    <v-tabs centered color="#16213e" slider-color="#16213e"
+      ><v-tab> Hello </v-tab>
+      <v-tab-item> <v-calendar-daily></v-calendar-daily> </v-tab-item
+      ><v-tab> Hello </v-tab> <v-tab-item> hawayu </v-tab-item
+      ><v-tab> Hello </v-tab> <v-tab-item> hawayu </v-tab-item
+      ><v-tab> Hello </v-tab> <v-tab-item> hawayu </v-tab-item></v-tabs
+    >
+  </v-sheet>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
